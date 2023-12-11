94bbaef92a2aeecb7c401f6f11c631d3e7ebf7ca
ashenye <luren145@gmail.com>
2023-12-11 22:36:27 +0800

    x

---
+++ dist/assets/profile-view-5cfbffba.js
@@ -0,0 +1,1 @@
+import{_ as e,e as t,o as c}from"./index-a7e509d8.js";const s={},o={class:"flex w-full h-full justify-center items-center"};function r(n,f){return c(),t("div",o,"个人档案")}const _=e(s,[["render",r]]);export{_ as default};
